523488e408ecf6d2fc7edd4bfb9148940af7f8ef
JacobD2013 <78101691+JacobD2013@users.noreply.github.com>
2024-03-25 12:45:19 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='Area and Volumes',
-    version='0.1',
+    version='0.2',
     packages=find_packages(),
     description='A Python library for solving areas and formulas',
     author='Jacob D',
